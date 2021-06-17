e25c1b3a7b6fc206a2c60fa7561de5704f15f4f7
Alexpogo471 <56255801+Alexpogo471@users.noreply.github.com>
2021-06-17 12:16:35 +0300

    add empty adapter, gson converter, recyclerView, item

--- app/src/main/java/ru/testAppMovie423432/themovie/MainActivity.kt
+++ app/src/main/java/ru/testAppMovie423432/themovie/MainActivity.kt
@@ -2,12 +2,16 @@ package ru.testAppMovie423432.themovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
     }
 }
