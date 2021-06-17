03063c3e1d87013eec2e8b3c05ae6b75e886d999
Alexpogo471 <56255801+Alexpogo471@users.noreply.github.com>
2021-06-17 11:29:51 +0300

    initial

---
+++ app/src/main/java/ru/testAppMovie423432/themovie/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.testAppMovie423432.themovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
